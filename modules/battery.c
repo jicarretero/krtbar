0440c5a854beaad28a8fafbe3ce48c240f4aa825
Jose Ignacio Carretero <joseignacio.carretero@gmail.com>
2024-02-25 21:47:13 +0100

    Added comprobation of existence of the battery

--- modules/battery.c
+++ modules/battery.c
@@ -1,6 +1,7 @@
 #include "../defs.h"
 #include "../util.h"
 #include <string.h>
+#include <unistd.h>
 
 int notified = 0;
 
@@ -9,30 +10,34 @@ void get_battery_state(char *buffer) {
   int mx[] = {90, 60, 40, 20, 10};
   int capacity;
   char status[12];
-  pscanf("/sys/class/power_supply/BAT0/capacity", "%d", &capacity);
-  pscanf("/sys/class/power_supply/BAT0/status", "%12[a-zA-Z ]", &status);
+  if (access("/sys/class/power_supply/BAT0/capacity", F_OK) == 0) {
+    printf(".... dando candela a los ficheros!");
+    pscanf("/sys/class/power_supply/BAT0/capacity", "%d", &capacity);
+    pscanf("/sys/class/power_supply/BAT0/status", "%12[a-zA-Z ]", &status);
 
-  char *r;
-  char *s;
+    char *r;
+    char *s;
 
-  for (int j = 0; j < 5; j++) {
-    if (capacity > mx[j]) {
-      r = bs[j];
-      break;
+    for (int j = 0; j < 5; j++) {
+      if (capacity > mx[j]) {
+        r = bs[j];
+        break;
+      }
     }
-  }
 
-  if (strncmp(status, "Discharging", 12) == 0) {
-    if (capacity < 15 && !notified) {
-      exec_command("notify-send -u critical \"Battery Too Low!\"", buffer);
-      notified = 1;
+    if (strncmp(status, "Discharging", 12) == 0) {
+      if (capacity < 15 && !notified) {
+        exec_command("notify-send -u critical \"Battery Too Low!\"", buffer);
+        notified = 1;
+      }
+      s = ""; /* "🔋"; */
+    } else {
+      notified = 0;
+      s = "";
     }
-    s = ""; /* "🔋"; */
+    /* char *s = "🔌 🔋   JIKONIK Hola mundo mundial!"; */
+    snprintf(buffer, MAX_COMPONENT_BUFFER, "%s %s  %d%%", s, r, capacity);
   } else {
-    notified = 0;
-    s = "";
+    snprintf(buffer, MAX_COMPONENT_BUFFER, " 🔌 - unknown");
   }
-
-  /* char *s = "🔌 🔋   JIKONIK Hola mundo mundial!"; */
-  snprintf(buffer, MAX_COMPONENT_BUFFER, "%s %s  %d%%", s, r, capacity);
 }
